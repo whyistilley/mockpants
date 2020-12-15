86b9d1f282d418f5f334b4494cc19e3bdc85df18
pat <ptilley@mailgun.com>
2020-12-14 21:15:20 -0600

    feat: rename Logo to SassBobMockPants and place image inside

---
+++ components/SassBobMockPants.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <img src='/images/mocking-spongebob.jpeg' alt='Mocking Spongebob' class='mockpants-image' />
+  </div>
+</template>
+
+<style>
+.mockpants-image {
+  animation: 2500ms appear;
+  margin: auto;
+  width: 50%;
+  height: auto;
+
+  @apply rounded-xl shadow-2xl mb-12;
+}
+
+@keyframes appear {
+  0% {
+    opacity: 0;
+  }
+}
+</style>
