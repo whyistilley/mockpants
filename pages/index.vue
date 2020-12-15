5dd3161042fad689a6db3eb29fb4ece24b1c185b
pat <ptilley@mailgun.com>
2020-12-14 21:17:17 -0600

    feat: implement Mutator component, remove unnecessary css

--- pages/index.vue
+++ pages/index.vue
@@ -1,34 +1,22 @@
 <template>
   <div class="container">
     <div>
-      <Logo />
-      <h1 class="title">
-        mockpants
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+      <h1 class='title'>Mockpants: The Spongebob Mocking Text Mutator</h1>
+      <SassBobMockPants />
+      <Mutator />
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+import SassBobMockPants from '~/components/SassBobMockPants'
+import Mutator from '~/components/Mutator'
+export default {
+  components: {
+    SassBobMockPants,
+    Mutator
+  }
+}
 </script>
 
 <style>
@@ -43,36 +31,8 @@ export default {}
   display: flex;
   justify-content: center;
   align-items: center;
-  text-align: center;
 }
-
 .title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+  @apply block uppercase tracking-wide text-gray-100 text-xl font-bold mb-4 underline text-center;
 }
 </style>
